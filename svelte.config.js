6b02c3adcadae3642b1b14df73ea0521e0aa491d
Robby Pelssers <robby.pelssers@engie.com>
2021-09-11 10:12:49 +0200

    build: add sass support

--- svelte.config.js
+++ svelte.config.js
@@ -5,17 +5,17 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: preprocess(),
+  preprocess: [preprocess({})],
 
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
     adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		})
+      // default options are shown
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+    }),
   },
 };
 
